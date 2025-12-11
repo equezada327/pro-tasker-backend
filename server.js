0c84e55b8e0cb0d7f503f2247ce3418d16ae2890
equezda327 <enrique.a.quezada@gmail.com>
2025-12-11 05:57:14 -0800

    Fix task routes path

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json({ extended: false })); // Parse JSON bodies
 // Define API routes
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use('/api/projects/:projectId/tasks', taskRoutes);
 
 // Health check endpoint
 app.get('/', (req, res) => {
